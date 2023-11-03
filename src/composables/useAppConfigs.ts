a0b9ec094fbf0a0825e881cd57a896b97f17eea3
Micah Xia <1962408497@qq.com>
2023-11-04 05:48:07 +0800

    refactor: 设置环境变量

--- src/composables/useAppConfigs.ts
+++ src/composables/useAppConfigs.ts
@@ -1,7 +1,18 @@
-import {appConfig} from "@/configs";
-
+import {appConfig, appConfigDev} from "@/configs";
 const useAppConfigs = () => {
-  return appConfig;
+  // #ifdef H5
+  // @ts-ignore
+  return import.meta.VITE_NODE_ENV === 'development' ? appConfigDev : appConfig;
+  // #endif
+
+  /**
+   * Vue3 非H5端，应直接访问 process.env.* 获取环境变量，不支持访问 process
+   * https://zh.uniapp.dcloud.io/tutorial/migration-to-vue3.html#%E7%8E%AF%E5%A2%83%E5%8F%98%E9%87%8F
+   */
+  // #ifndef H5
+  // @ts-ignore
+  return process.env.VITE_NODE_ENV === 'development' ? appConfigDev : appConfig;
+  // #endif
 }
 
 export default useAppConfigs;
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,3 +2,8 @@ export const appConfig = {
   baseUrl: 'http:localhost:4001',
   timeout: 60 * 60 * 1000
 }
+
+export const appConfigDev = {
+  baseUrl: 'http:localhost:4001',
+  timeout: 60 * 60 * 1000
+}
