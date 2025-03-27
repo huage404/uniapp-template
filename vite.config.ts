86978cb23a22dc814dd8f68703ea44813d14a13c
Rowan <1962408497@qq.com>
2025-03-28 07:25:48 +0800

    ✨ feat(ui): 全局注册 Wot Design Uni 组件，并配置自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import {defineConfig} from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
+import Components from '@uni-helper/vite-plugin-uni-components'
+import {WotResolver} from '@uni-helper/vite-plugin-uni-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [uni()],
+	plugins: [
+		Components({
+			resolvers: [WotResolver()],
+		}),
+		uni(),
+	],
 })
