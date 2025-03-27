3b06176f6f3db7747e58ffc1a0af60e6dee5c13b
Rowan <1962408497@qq.com>
2025-03-28 04:34:19 +0800

    :tada: chore: 初始化项目结构
    配置 ESLint + Stylelint + Prettier + Commitlint

---
+++ stylelint.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: ['stylelint-config-standard-scss'],
+	rules: {
+		// 为颜色函数指定现代或传统的符号。
+		'color-function-notation': ['legacy', {'ignore': ['with-var-inside']}],
+		// 指定 alpha 值的百分比或数字符号。
+		'alpha-value-notation': 'number',
+	},
+}
